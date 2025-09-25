01e88a8d3142d4df4638c8e243bc2186d5a0e2cb
rylander925 <rylander925@gmail.com>
2025-09-24 20:44:17 -0700

    Added InputMovies function

--- main.cpp
+++ main.cpp
@@ -16,6 +16,8 @@ Also code a print() method which prints the object data in a simple format.
 
 using namespace std;
 
+const int STREAM_IGNORE_CHARS = 100;
+
 class Movie {
     public:
         //getters & setters
@@ -26,10 +28,57 @@ class Movie {
         int getYear() const           { return year; }
         string getTitle() const       { return title; }
 
-        void print();
+        //print movie info
+        void print() const;
 
     private:
         string writer;
         int year;
         string title;
 };
+
+void InputMovies(vector<Movie>& movies, string filename);
+
+int main() {
+    
+}
+
+void Movie::print() const {
+    cout << "Movie: " << title << endl;
+    cout << "\tYear: " << year << endl;
+    cout << "\tScreen writer: " << writer << endl;
+}
+
+/**
+ * Retrieve movie data from file into a vector of movies 
+ * @param movies Vector to input data to
+ * @param filename File to retrieve movie data from
+ */
+void InputMovies(vector<Movie>& movies, string filename) {
+    ifstream infile;
+    string title;
+    string writer;
+    int year;
+    Movie newMovie;
+
+    infile.open(filename);
+    if (!infile.good()) {
+        cout << "ERROR: Could not open file \"" << filename << "\"" << endl;
+        throw ios_base::failure("Invalid file name"); 
+    }
+
+    if (infile.good()) {
+        while(getline(infile, title)) {
+            getline(infile, writer);
+            infile >> year;
+
+            newMovie.setTitle(title);
+            newMovie.setWriter(writer);
+            newMovie.setYear(year);
+
+            movies.push_back(newMovie);
+
+            infile.ignore(STREAM_IGNORE_CHARS, '\n');
+        }
+    }
+}
